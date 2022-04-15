17290f408edc107c5eb794f4f7852e02e0362292
veronicabets <98438893+veronicabets@users.noreply.github.com>
2022-04-15 11:30:54 -0400

    Added axios integration to show weather

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+import { Hearts } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "8539eaf09e0166b7c856b19fe9896cfc";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <Hearts color="#00BFFF" height={80} width={80} />;
+}
