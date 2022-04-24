0360e5b677ac42802cc874402aa06ab9f9dda0be
veronicabets <98438893+veronicabets@users.noreply.github.com>
2022-04-24 11:13:39 -0400

    added icons api

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,16 +17,14 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temperature">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
             <div className="float-left">
               <strong>{Math.round(props.data.temperature)}</strong>
               <span className="units">
                 <small>
-                  <a href="/">°C</a>|<a href="/">°F</a>
+                  <a href="/">°C</a> |<a href="/">°F</a>
                 </small>
               </span>
             </div>
