7d48e56b2251f51b059931fba1c8a80a2de3cbcb
veronicabets <98438893+veronicabets@users.noreply.github.com>
2022-04-29 17:37:06 -0400

    edited css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -28,8 +28,10 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {Math.round(props.data.humidity)}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/hr</li>
+            <li className="humidity">
+              Humidity: {Math.round(props.data.humidity)}%
+            </li>
+            <li className="wind">Wind: {Math.round(props.data.wind)} km/hr</li>
           </ul>
         </div>
       </div>
